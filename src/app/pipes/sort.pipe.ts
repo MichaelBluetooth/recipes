12f9b35b683af79483173cff53ac6208c3ee8e25
Michael Bluetooth <matthew.john.bischoff@gmail.com>
2018-09-24 20:50:27 -0400

    better sort pipe:

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -6,9 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SortPipe implements PipeTransform {
     transform(array: any[], field: string): any[] {
         array.sort((a: any, b: any) => {
-            if (a[field] < b[field] || false === b.hasOwnProperty(field)) {
+            const aValue = a[field] ? a[field] : null;
+            const bValue = b[field] ? b[field] : null;
+            if (aValue < bValue ) {
                 return -1;
-            } else if (a[field] > b[field] || false === a.hasOwnProperty(field)) {
+            } else if (aValue > bValue) {
                 return 1;
             } else {
                 return 0;
