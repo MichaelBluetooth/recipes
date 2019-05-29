6fbc19ff9695aaec6b28e4a6562b1d515d7a4fb5
Matthew Bischoff <matthew.john.bischoff@gmail.com>
2019-05-28 23:08:12 -0400

    Added button to enable searches by barcode

--- src/app/components/grocery/grocery-item-list/grocery-item-list.component.ts
+++ src/app/components/grocery/grocery-item-list/grocery-item-list.component.ts
@@ -1,6 +1,10 @@
+import { GroceryPackage } from './../../../models/grocerypackage';
+import { BarcodeScannerService } from './../../../services/barcode-scanner/barcode-scanner.service';
 import { GroceryItem } from './../../../models/groceryitem';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { distinctUntilChanged } from 'rxjs/operators';
+import { GroceryPackageService } from 'src/app/services/grocery/grocery-package.service';
 
 @Component({
   selector: 'app-grocery-item-list',
@@ -10,12 +14,34 @@ import { ActivatedRoute } from '@angular/router';
 export class GroceryItemListComponent implements OnInit {
 
   groceryItems: GroceryItem[] = [];
+  scannerRunning = false;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private packagesService: GroceryPackageService,
+    private barcodeScannerService: BarcodeScannerService) { }
 
   ngOnInit() {
     this.activatedRoute.data.subscribe((routeData) => {
       this.groceryItems = routeData.groceryitems;
     });
+  }
+
+  startScanner() {
+    this.scannerRunning = true;
+    this.barcodeScannerService.startScanner('#scanner-container')
+      .pipe(distinctUntilChanged())
+      .subscribe(barcodeSearch => {
+        this.barcodeScannerService.stopScanner();
+        this.scannerRunning = false;
+
+        this.packagesService.searchGroceryPackages({ 'groceryItem.barcode': barcodeSearch }).then((pkgs: GroceryPackage[]) => {
+          if (pkgs && pkgs.length > 0) {
+            this.router.navigate(['groceryitems', pkgs[0].groceryItem._id]);
+          } else {
+            alert(`No items matching barcode ${barcodeSearch} found`);
+          }
+        });
+      });
   }
 }
